1dfeee84fa30a991a81ec168f612c886a93828ad
Lawliet-Chan <1576710154@qq.com>
2022-01-16 13:06:39 +0800

    add poa verify_block test

--- apps/poa/poa_test.go
+++ apps/poa/poa_test.go
@@ -3,22 +3,34 @@ package poa
 import (
 	"github.com/stretchr/testify/assert"
 	. "github.com/yu-org/yu/common"
+	. "github.com/yu-org/yu/core/keypair"
+	. "github.com/yu-org/yu/core/types"
 	"testing"
 )
 
+var (
+	myPubkey1, myPubkey2, myPubkey3    PubKey
+	myPrivkey1, myPrivkey2, myPrivkey3 PrivKey
+	validators                         []ValidatorAddrIp
+	node1, node2, node3                *Poa
+)
+
+func initGlobalVars() {
+	myPubkey1, myPrivkey1, validators = InitKeypair(0)
+	node1 = NewPoa(myPubkey1, myPrivkey1, validators)
+	println("addr1 = ", myPubkey1.Address().String())
+
+	myPubkey2, myPrivkey2, validators = InitKeypair(1)
+	node2 = NewPoa(myPubkey2, myPrivkey2, validators)
+	println("addr2 = ", myPubkey2.Address().String())
+
+	myPubkey3, myPrivkey3, validators = InitKeypair(2)
+	node3 = NewPoa(myPubkey3, myPrivkey3, validators)
+	println("addr3 = ", myPubkey3.Address().String())
+}
+
 func TestCompeteLeader(t *testing.T) {
-	myPubkey1, myPrivkey1, validators1 := InitKeypair(0)
-	node1 := NewPoa(myPubkey1, myPrivkey1, validators1)
-	t.Log("addr1 = ", myPubkey1.Address().String())
-
-	myPubkey2, myPrivkey2, validators2 := InitKeypair(1)
-	node2 := NewPoa(myPubkey2, myPrivkey2, validators2)
-	t.Log("addr2 = ", myPubkey2.Address().String())
-
-	myPubkey3, myPrivkey3, validators3 := InitKeypair(2)
-	node3 := NewPoa(myPubkey3, myPrivkey3, validators3)
-	t.Log("addr3 = ", myPubkey3.Address().String())
-
+	initGlobalVars()
 	for i := 1; i <= 30; i++ {
 		bn := BlockNum(i)
 		t.Log("block number = ", bn)
@@ -36,4 +48,43 @@ func TestCompeteLeader(t *testing.T) {
 			assert.Equal(t, myPubkey3.Address().String(), addr3.String(), "addr = %s", addr3.String())
 		}
 	}
+}
+
+func TestVerifyBlock(t *testing.T) {
+	initGlobalVars()
+
+	miner := myPubkey1
+	signer, err := myPrivkey1.SignData(NullHash.Bytes())
+	if err != nil {
+		t.Fatal("sign blockhash error: ", err)
+	}
+
+	block := &CompactBlock{
+		Header: &Header{
+			ChainID:        0,
+			PrevHash:       NullHash,
+			Hash:           NullHash,
+			Height:         0,
+			TxnRoot:        NullHash,
+			StateRoot:      NullHash,
+			Timestamp:      0,
+			PeerID:         "",
+			Extra:          nil,
+			LeiLimit:       0,
+			LeiUsed:        0,
+			MinerPubkey:    miner.BytesWithType(),
+			MinerSignature: signer,
+			Validators:     nil,
+			ProofBlockHash: NullHash,
+			ProofHeight:    0,
+			Proof:          nil,
+			Nonce:          0,
+			Difficulty:     0,
+		},
+		TxnsHashes: nil,
+	}
+
+	assert.True(t, node1.VerifyBlock(block))
+	assert.True(t, node2.VerifyBlock(block))
+	assert.True(t, node3.VerifyBlock(block))
 }
