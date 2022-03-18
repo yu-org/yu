cbe24a780c81c99889565afd09bf4d5974840a9c
Lawliet-Chan <1576710154@qq.com>
2022-03-18 20:25:06 +0800

    add yu_conf and modify build.sh

--- example/poa/build.sh
+++ example/poa/build.sh
@@ -1,13 +1,17 @@
 go build -v -o poa
 
-cp poa ~/run-yu/node1/
+node1_path=~/run-yu/node1
+node2_path=~/run-yu/node2
+node3_path=~/run-yu/node3
 
-cp poa ~/run-yu/node2/
+mkdir -p $node1_path
+mkdir -p $node2_path
+mkdir -p $node3_path
 
-mv poa ~/run-yu/node3/
+cp poa $node1_path/
+cp poa $node2_path/
+mv poa $node3_path/
 
-rm -f ~/run-yu/node1/*.db
-
-rm -f ~/run-yu/node2/*.db
-
-rm -f ~/run-yu/node3/*.db
+rm -f $node1_path/*.db
+rm -f $node2_path/*.db
+rm -f $node3_path/*.db
