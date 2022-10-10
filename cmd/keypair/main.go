b511202848605228fbbf95d1c2e63be7d825187d
Lawliet-Chan <1576710154@qq.com>
2022-10-10 21:31:12 +0800

    add keypair cmd

---
+++ cmd/keypair/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yu-org/yu/core/keypair"
+	"os"
+)
+
+func main() {
+	keyType := os.Args[1]
+	secret := os.Args[2]
+	pubkey, privkey, err := keypair.GenKeyPairWithSecret(keyType, []byte(secret))
+	if err != nil {
+		fmt.Println("generate keypair failed: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("public key: ", pubkey.String())
+	fmt.Println("public key with type: ", pubkey.StringWithType())
+	fmt.Println("private key: ", privkey.String())
+	fmt.Println("private key with type: ", privkey.StringWithType())
+}
