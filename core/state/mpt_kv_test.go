aaf07aec2de5ed492ae564e30460918b0ba5af1b
xinran chen <lawliet@xinran-m1x.local>
2023-07-05 01:02:15 +0800

    trie etst

--- core/state/mpt_kv_test.go
+++ core/state/mpt_kv_test.go
@@ -1,6 +1,7 @@
 package state
 
 import (
+	"github.com/stretchr/testify/assert"
 	. "github.com/yu-org/yu/common"
 	"github.com/yu-org/yu/config"
 	"github.com/yu-org/yu/infra/storage/kv"
@@ -8,32 +9,40 @@ import (
 	"testing"
 )
 
-var TestStateKvCfg = &config.MptKvConf{
-	IndexDB: config.KVconf{KvType: "bolt", Path: "./state_index.db", Hosts: nil},
-	NodeBase: config.KVconf{
-		KvType: "bolt",
-		Path:   "./state_base.db",
-		Hosts:  nil,
-	},
+var kvcfg = &config.KVconf{
+	KvType: "bolt",
+	Path:   "./test-mpt-kv.db",
+	Hosts:  nil,
 }
 
-type TestTripod struct{}
+var (
+	key1   = []byte("dayu-key1")
+	value1 = []byte("dayu-value1")
+	key2   = []byte("dayu-key2")
+	value2 = []byte("dayu-value2")
+)
 
-func (tt *TestTripod) Name() string {
-	return "test-tripod"
+type TestTripod1 struct{}
+
+func (tt *TestTripod1) Name() string {
+	return "test-tripod-1"
+}
+
+type TestTripod2 struct{}
+
+func (TestTripod2) Name() string {
+	return "test-tripod-2"
 }
 
 func TestKvCommit(t *testing.T) {
-	kvdb, err := kv.NewKvdb(&config.KVconf{
-		KvType: "bolt",
-		Path:   "./test-mpt-kv.db",
-		Hosts:  nil,
-	})
+	kvdb, err := kv.NewKvdb(kvcfg)
 	statekv := NewMptKV(kvdb)
 
-	tri := &TestTripod{}
+	tri1 := &TestTripod1{}
+	tri2 := &TestTripod2{}
 
-	statekv.Set(tri, []byte("dayu-key"), []byte("dayu-value"))
+	statekv.Set(tri1, key1, value1)
+	statekv.Set(tri2, key2, value2)
 
 	statekv.NextTxn()
 
@@ -46,22 +55,25 @@ func TestKvCommit(t *testing.T) {
 
 	t.Logf("state-root is %s", stateRoot.String())
 
-	value, err := statekv.Get(tri, []byte("dayu-key"))
-	if err != nil {
-		t.Fatalf("get state-kv error: %s", err.Error())
-	}
-	t.Logf("get value is %s", string(value))
+	value, err := statekv.Get(tri1, key1)
+	assert.NoError(t, err, "get key1 state-kv error")
+	assert.Equal(t, value1, value)
 
-	value, err = statekv.GetByBlockHash(tri, []byte("dayu-key"), NullHash)
-	if err != nil {
-		t.Fatalf("get state-kv by blockHash error: %s", err.Error())
-	}
-	t.Logf("get value by blockHash is %s", string(value))
+	value, err = statekv.Get(tri2, key2)
+	assert.NoError(t, err, "get key2 state-kv error")
+	assert.Equal(t, value2, value)
+
+	value, err = statekv.GetByBlockHash(tri1, key1, NullHash)
+	assert.NoError(t, err, "get key1 state-kv by blockHash error")
+	assert.Equal(t, value1, value)
+
+	value, err = statekv.GetByBlockHash(tri2, key2, NullHash)
+	assert.NoError(t, err, "get key2 state-kv by blockHash error")
+	assert.Equal(t, value2, value)
 
 	removeTestDB()
 }
 
 func removeTestDB() {
-	os.RemoveAll(TestStateKvCfg.NodeBase.Path)
-	os.RemoveAll(TestStateKvCfg.IndexDB.Path)
+	os.RemoveAll(kvcfg.Path)
 }
