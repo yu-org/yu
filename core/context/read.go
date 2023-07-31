a830c07f4491112975f189242b577fa0aedab2b8
xinran chen <lawliet@xinran-m1x.local>
2023-07-31 10:41:14 +0800

    fix

--- core/context/read.go
+++ core/context/read.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/yu-org/yu/common"
 	"github.com/yu-org/yu/core"
+	"net/http"
 )
 
 type ReadContext struct {
@@ -18,4 +19,12 @@ func NewReadContext(ctx *gin.Context) (*ReadContext, error) {
 
 func (rc *ReadContext) GetBlockHash() common.Hash {
 	return common.HexToHash(rc.Query(core.BlockHashKey))
+}
+
+func (rc *ReadContext) JsonOk(v any) {
+	rc.JSON(http.StatusOK, v)
+}
+
+func (rc *ReadContext) StringOk(format string, values ...any) {
+	rc.String(http.StatusOK, format, values)
 }
