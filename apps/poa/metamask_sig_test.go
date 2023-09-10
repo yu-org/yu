e5f3c1dd902d644a1d1b57bd537863b787f0457b
Lawliet-Chan <1576710154@qq.com>
2023-09-10 21:08:13 +0800

    fix

--- apps/poa/metamask_sig_test.go
+++ apps/poa/metamask_sig_test.go
@@ -10,10 +10,11 @@ func TestSig(t *testing.T) {
 	wrCall := common.WrCall{
 		TripodName: "question",
 		FuncName:   "AddQuestion",
-		Params:     `"title":"aaa","content":"bbb","timestamp":1692652052812}`,
+		Params:     `{"title":"aaa","content":"bbb","timestamp":1692652052812}`,
 		LeiPrice:   0,
 		Tips:       0,
 	}
+	t.Log(wrCall)
 	hash, err := wrCall.Hash()
 	assert.NoError(t, err)
 	t.Logf("%x", hash)
