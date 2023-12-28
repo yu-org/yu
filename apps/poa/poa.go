7faa48ad921ec0ba0887f7cc91854de5739299e7
Xinran <1576710154@qq.com>
2023-12-28 15:37:22 +0800

    disable checkTxn from poa (#106)

--- apps/poa/poa.go
+++ apps/poa/poa.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/sirupsen/logrus"
-	"github.com/yu-org/yu/apps/metamask"
 	. "github.com/yu-org/yu/common"
 	. "github.com/yu-org/yu/core/keypair"
 	. "github.com/yu-org/yu/core/tripod"
@@ -92,7 +91,8 @@ func (h *Poa) LocalAddress() Address {
 }
 
 func (h *Poa) CheckTxn(txn *SignedTxn) error {
-	return metamask.CheckMetamaskSig(txn)
+	// return metamask.CheckMetamaskSig(txn)
+	return nil
 }
 
 func (h *Poa) VerifyBlock(block *Block) bool {
