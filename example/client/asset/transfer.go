0cd0feb6bd6f7a8a1e69c70f37b581e8bc92bfda
Lawliet-Chan <1576710154@qq.com>
2024-08-06 19:20:35 +0800

    try

--- example/client/asset/transfer.go
+++ example/client/asset/transfer.go
@@ -67,7 +67,7 @@ func CreateAccount(privkey PrivKey, pubkey PubKey, amount uint64) {
 	}
 	postBody := &core.WritingPostBody{
 		Pubkey:    pubkey.StringWithType(),
-		Address:   hexutil.Encode(pubkey.Address().Bytes()),
+		Address:   pubkey.Address().String(),
 		Signature: hexutil.Encode(sig),
 		Call:      wrCall,
 	}
@@ -109,7 +109,7 @@ func TransferBalance(privkey PrivKey, pubkey PubKey, to Address, amount, leiPric
 	postBody := &core.WritingPostBody{
 		Pubkey:    pubkey.StringWithType(),
 		Signature: hexutil.Encode(sig),
-		Address:   hexutil.Encode(pubkey.Address().Bytes()),
+		Address:   pubkey.Address().String(),
 		Call:      wrCall,
 	}
 	err = CallChainByWriting(postBody)
