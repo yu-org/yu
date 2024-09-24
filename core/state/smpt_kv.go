aa84c1110a0430c32ce0d5d8022375816d9a8ce5
Lawliet-Chan <1576710154@qq.com>
2024-09-24 18:21:47 +0800

    add print

--- core/state/smpt_kv.go
+++ core/state/smpt_kv.go
@@ -235,6 +235,7 @@ func (skv *SpmtKV) FinalizeBlock(block *types.Block) {
 
 func (skv *SpmtKV) setIndexDB(block *types.Block, stateRoot []byte) error {
 	fmt.Println("spmtKV indexDB is ", skv.indexDB)
+	fmt.Println("block is", block.Hash)
 	return skv.indexDB.Set(block.Hash.Bytes(), stateRoot)
 }
 
