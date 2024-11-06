08bf94fddb90b7fa3c2f8e645ce2865a2ae9fc1f
Lawliet-Chan <1576710154@qq.com>
2024-11-06 23:30:56 +0800

    add getParams for readcontext

--- core/context/read.go
+++ core/context/read.go
@@ -1,7 +1,9 @@
 package context
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/yu-org/yu/common"
+	"github.com/yu-org/yu/common/yerror"
 	"net/http"
 )
 
@@ -40,6 +42,44 @@ func (rc *ReadContext) Response() *ResponseData {
 
 func (rc *ReadContext) BindJson(v any) error {
 	return common.BindJsonParams(rc.rdCall.Params, v)
+}
+
+func (rc *ReadContext) GetParams(key string) any {
+	value, err := rc.TryGetParams(key)
+	if err != nil {
+		logrus.Panicf("ReadContext.GetParams() failed: %v", err)
+	}
+	return value
+}
+
+func (rc *ReadContext) TryGetParams(key string) (any, error) {
+	params := make(map[string]any)
+	err := rc.BindJson(params)
+	if err != nil {
+		return nil, err
+	}
+	return params[key], nil
+}
+
+func (rc *ReadContext) GetString(key string) string {
+	value, err := rc.TryGetString(key)
+	if err != nil {
+		logrus.Panicf("ReadContext.GetString() failed: %v", err)
+	}
+	return value
+}
+
+func (rc *ReadContext) TryGetString(key string) (string, error) {
+	params := make(map[string]any)
+	err := rc.BindJson(params)
+	if err != nil {
+		return "", err
+	}
+	value := params[key]
+	if _, ok := value.(string); !ok {
+		return "", yerror.TypeErr
+	}
+	return value.(string), nil
 }
 
 func (rc *ReadContext) GetBlockHash() *common.Hash {
