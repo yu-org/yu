93007a30069b05c819b87c2cd856b80c6d0aa69f
ReyisaRuby <yisa@reddio.com>
2024-12-24 16:52:26 +0800

    revise sql
    Signed-off-by: ReyisaRuby <yisa@reddio.com>

--- infra/storage/sql/sqlite.go
+++ infra/storage/sql/sqlite.go
@@ -1,28 +1,41 @@
 package sql
 
 import (
-	"github.com/yu-org/yu/infra/storage"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"log"
 	"os"
 	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"github.com/yu-org/yu/config"
+	"github.com/yu-org/yu/infra/storage"
 )
 
 type Sqlite struct {
 	*gorm.DB
 }
 
-func NewSqlite(dsn string) (*Sqlite, error) {
+func NewSqlite(cfg *config.SqlDbConf) (*Sqlite, error) {
 	newLogger := logger.New(
 		log.New(os.Stdout, "/r/n", log.LstdFlags), logger.Config{SlowThreshold: time.Second})
-	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{
+	db, err := gorm.Open(sqlite.Open(cfg.Dsn), &gorm.Config{
 		Logger:          newLogger,
 		CreateBatchSize: 50000,
 	})
 	if err != nil {
 		return nil, err
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	if cfg.MaxOpenConnections > 0 {
+		sqlDB.SetMaxOpenConns(cfg.MaxOpenConnections)
+	}
+	if cfg.MaxIdleConnections > 0 {
+		sqlDB.SetMaxIdleConns(cfg.MaxIdleConnections)
 	}
 	return &Sqlite{db}, nil
 }
